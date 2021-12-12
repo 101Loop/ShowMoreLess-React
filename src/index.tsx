4aacf1541faaf23ca12bc4e246967b24e9ec01be
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-12-12 14:09:46 +0530

    adds onExpand function

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react'
-import { useState } from 'react';
 import styles from './styles.module.css';
 
-interface ShowMoreLess {
+type OnExpand = (value: boolean) => void;
+
+type ClassesObject = {
+  root?: string,
+  text?: string,
+  clickable?: string,
+}
+
+type ShowMoreLessProps = {
   text: string,
   showMoreLabel?: string,
   showLessLabel?: string,
-  classes?: any,
+  classes?: ClassesObject,
   threshold?: number,
+  expanded?: boolean,
+  onExpand?: OnExpand,
 }
 
-function ShowMoreLess({ text, classes, showLessLabel = 'Show Less', showMoreLabel = '...Show More', threshold = 100 }: ShowMoreLess) {
-  const [expanded, setExpanded] = useState(false);
-  const clickableClass = classes?.clickable || styles.clickable;
+function ShowMoreLess({ text, classes, showLessLabel = 'Show Less', showMoreLabel = '...Show More', threshold = 100, expanded = false, onExpand }: ShowMoreLessProps) {
+  const clickableClass = [classes?.clickable || styles.clickable];
+  if (onExpand) {
+    clickableClass.push(styles.pointer);
+  }
 
   return (
     <div className={classes?.root}>
@@ -20,9 +31,11 @@ function ShowMoreLess({ text, classes, showLessLabel = 'Show Less', showMoreLabe
         {expanded ? text : text.substring(0, threshold)}
       </span>
       {
-        <span style={{
-          cursor: 'pointer'
-        }} className={clickableClass} onClick={() => setExpanded(!expanded)}>{expanded ? showLessLabel : showMoreLabel}</span>
+        <span className={clickableClass.join(' ')} onClick={() => {
+          if (onExpand) {
+            onExpand(!expanded);
+          }
+        }}>{expanded ? showLessLabel : showMoreLabel}</span>
       }
     </div>);
 }
